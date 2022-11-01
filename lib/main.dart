915c60049b607789dbb2790f0f06d6336760b50c
mofikry <mofikry17@gmail.com>
2022-11-01 22:51:41 +0200

    initial commit

---
+++ lib/main.dart
@@ -0,0 +1,24 @@
+import 'package:e_commerce/services/dio_helper.dart';
+import 'package:flutter/material.dart';
+
+import 'features/login/presentation/views/login_screen.dart';
+
+void main() {
+  DioHelper.init();
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demoooooo',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(scaffoldBackgroundColor: const Color(0xffF9F9F9)),
+      home: LoginScreen(),
+    );
+  }
+}
