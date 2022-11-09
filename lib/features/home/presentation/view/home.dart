3ae844a068b1f1a228c555f26804218d772dcb20
mofikry <mofikry17@gmail.com>
2022-11-09 21:42:56 +0200

    last

--- lib/features/home/presentation/view/home.dart
+++ lib/features/home/presentation/view/home.dart
@@ -1,24 +1,49 @@
-import 'package:e_commerce/core/network/cache_helper.dart';
-import 'package:e_commerce/features/login/presentation/views/login_screen.dart';
+import 'package:carousel_slider/carousel_slider.dart';
+import 'package:conditional_builder_null_safety/conditional_builder_null_safety.dart';
+import 'package:e_commerce/features/home/data/model_home.dart';
+import 'package:e_commerce/features/home/presentation/view_model/cubit/home_cubit.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-        body: Center(
-            child: TextButton(
-      onPressed: () {
-        CacheHelper.removeData(key: 'token').then((value) {
-          if (value) {
-            Navigator.pushReplacement(context,
-                MaterialPageRoute(builder: (context) => LoginScreen()));
-          }
+    return BlocConsumer<HomeCubit, HomeState>(
+        listener: (context, state) {},
+        builder: (context, state) {
+          return ConditionalBuilder(
+              condition: HomeCubit.get(context).homeModel != null,
+              builder: (context) =>
+                  ProductBuilder(HomeCubit.get(context).homeModel),
+              fallback: (context) => const Center(
+                    child: CircularProgressIndicator(),
+                  ));
         });
-      },
-      child: Text('Logout'),
-    )));
   }
+}
+
+Widget ProductBuilder(HomeModel? model) {
+  return Column(
+    children: [
+      CarouselSlider(
+          items: model!.data.banners
+              .map((e) => Image(
+                    image: NetworkImage(e.image),
+                    width: double.infinity,
+                    fit: BoxFit.fill,
+                  ))
+              .toList(),
+          options: CarouselOptions(
+            viewportFraction: 1,
+            height: 250,
+            autoPlay: true,
+            initialPage: 0,
+            enableInfiniteScroll: true,
+            reverse: false,
+            autoPlayInterval: const Duration(seconds: 3),
+          ))
+    ],
+  );
 }
